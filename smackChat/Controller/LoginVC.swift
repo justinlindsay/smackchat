9989d56c103824d582166b291624ec68cb9dc34c
Justin Lindsay <justinlindsay@Justins-iMac.local>
2017-07-25 23:34:03 -0400

    Login UI set up.

---
+++ smackChat/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  smackChat
+//
+//  Created by Justin Lindsay on 7/25/17.
+//  Copyright © 2017 Justin Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+}
---
+++ smackChat/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  smackChat
+//
+//  Created by Justin Lindsay on 7/25/17.
+//  Copyright © 2017 Justin Lindsay. All rights reserved.
+//
+
+import Foundation
+
+
+// Segues
+let TO_LOGIN = "toLogin"
