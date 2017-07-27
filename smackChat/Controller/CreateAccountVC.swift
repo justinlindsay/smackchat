11983f6d82154f80b59ed88a720e7b6c3c89d15a
Justin Lindsay <justinlindsay@Justins-iMac.local>
2017-07-27 17:42:36 -0400

    added create a user functionality

--- smackChat/Controller/CreateAccountVC.swift
+++ smackChat/Controller/CreateAccountVC.swift
@@ -17,11 +17,17 @@ class CreateAccountVC: UIViewController {
     @IBOutlet weak var emailTxt: UITextField!
     @IBOutlet weak var userImg: UIImageView!
     
+    // Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func createAccntPressed(_ sender: Any) {
+        
+        guard let name = usernameTxt.text , usernameTxt.text != "" else { return }
         
         guard let email = emailTxt.text , emailTxt.text != "" else { return }
         
@@ -35,7 +41,14 @@ class CreateAccountVC: UIViewController {
                     
                     if success {
                         
-                        print("logged in user!", AuthService.instance.authToken)
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            
+                            if success {
+                                
+                                print(UserDataService.instance.name, UserDataService.instance.avatarName)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
                         
                     }
                 })
---
+++ smackChat/Services/UserDataService.swift
@@ -0,0 +1,36 @@
+//
+//  UserDataService.swift
+//  smackChat
+//
+//  Created by Justin Lindsay on 7/27/17.
+//  Copyright © 2017 Justin Lindsay. All rights reserved.
+//
+
+import Foundation
+
+class UserDataService {
+    
+    static let instance = UserDataService()
+    
+    public private(set) var id = ""
+    public private(set) var avatarColor = ""
+    public private(set) var avatarName = ""
+    public private(set) var email = ""
+    public private(set) var name = ""
+    
+    func setUserData(id: String, color: String, avatarName: String, email: String, name: String) {
+        
+        self.id = id
+        self.avatarColor = color
+        self.avatarName = avatarName
+        self.email = email
+        self.name = name
+        
+    }
+    
+    func setAvatarName(avatarName: String) {
+        
+        self.avatarName = avatarName
+        
+    }
+}
