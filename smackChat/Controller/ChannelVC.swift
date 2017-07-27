e04f40a58be4a820d4ac7f1757454b9b8a8fd9f7
Justin Lindsay <justinlindsay@Justins-iMac.local>
2017-07-27 19:39:11 -0400

    Login interface

--- smackChat/Controller/ChannelVC.swift
+++ smackChat/Controller/ChannelVC.swift
@@ -14,13 +14,30 @@ class ChannelVC: UIViewController {
     
     @IBOutlet weak var loginBtn: UIButton!
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){}
+    @IBOutlet weak var userImg: CircleImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
-
+    
+    @objc func userDataDidChange(_ notif: Notification) {
+        
+        if AuthService.instance.isLoggedIn {
+            
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            
+        } else {
+            
+            loginBtn.setTitle("Login", for: .normal)
+            userImg.image = UIImage(named: "menuProfileIcon")
+            userImg.backgroundColor = UIColor.clear
+            
+        }
+    }
     @IBAction func loginBtnPressed(_ sender: UIButton) {
         
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
