1c42d33ba4fbff5eba97c171dacd45e6554b1a6c
Justin Lindsay <justinlindsay@Justins-iMac.local>
2017-07-27 18:26:54 -0400

    finished avatar picking functionality

--- smackChat/View/AvatarCell.swift
+++ smackChat/View/AvatarCell.swift
@@ -8,6 +8,13 @@
 
 import UIKit
 
+enum AvatarType {
+    
+    case dark
+    case light
+    
+}
+
 class AvatarCell: UICollectionViewCell {
     
     // Outlets
@@ -20,6 +27,20 @@ class AvatarCell: UICollectionViewCell {
         
     }
     
+    func configureCell(index: Int, type: AvatarType) {
+        
+        if type == AvatarType.dark {
+            
+            avatarImg.image = UIImage(named: "dark\(index)")
+            self.layer.backgroundColor = UIColor.lightGray.cgColor
+            
+        } else {
+            
+            avatarImg.image = UIImage(named: "light\(index)")
+            self.layer.backgroundColor = UIColor.gray.cgColor
+            
+        }
+    }
     func setUpView() {
         
         self.layer.backgroundColor = UIColor.lightGray.cgColor
