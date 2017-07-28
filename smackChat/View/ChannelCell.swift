175f54cd373e90609ce3b64434b32c0391643c1a
Justin Lindsay <justinlindsay@Justins-iMac.local>
2017-07-27 22:20:35 -0400

    created channel table view cells

---
+++ smackChat/View/ChannelCell.swift
@@ -0,0 +1,43 @@
+//
+//  ChannelCell.swift
+//  smackChat
+//
+//  Created by Justin Lindsay on 7/27/17.
+//  Copyright © 2017 Justin Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCell: UITableViewCell {
+
+    // Outlets
+    @IBOutlet weak var channelName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        if selected {
+            
+            self.layer.backgroundColor = UIColor(white: 1, alpha: 0.2).cgColor
+            
+        } else {
+            
+            self.layer.backgroundColor = UIColor.clear.cgColor
+            
+        }
+    }
+    
+    func configureCell(channel: Channel) {
+        
+        let title = channel.channelTitle ?? ""
+        channelName.text = "#\(title)"
+        
+    }
+    
+
+}
