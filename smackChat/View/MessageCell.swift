b0c36cb38af40900c2c980b434ece640f7a1a63c
Justin Lindsay <justinlindsay@Justins-iMac.local>
2017-07-28 17:50:22 -0400

    displaying messages and dynamically fixing cell height for chat size

---
+++ smackChat/View/MessageCell.swift
@@ -0,0 +1,35 @@
+//
+//  MessageCell.swift
+//  smackChat
+//
+//  Created by Justin Lindsay on 7/28/17.
+//  Copyright © 2017 Justin Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    // Outlets
+    @IBOutlet weak var userImg: CircleImage!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var timeStampLbl: UILabel!
+    @IBOutlet weak var messageBodyLbl: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+    
+    func configureCell(message: Message) {
+        
+        messageBodyLbl.text = message.message
+        userNameLbl.text = message.userName
+        userImg.image = UIImage(named: message.userAvatar)
+        userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+        
+    }
+
+}
