45bafb615ad9a56c1a0aa9e6b0a75f0db070ad34
Justin Lindsay <justinlindsay@Justins-iMac.local>
2017-07-27 20:10:04 -0400

    created profile view

--- smackChat/Controller/ChannelVC.swift
+++ smackChat/Controller/ChannelVC.swift
@@ -41,6 +41,16 @@ class ChannelVC: UIViewController {
     }
     @IBAction func loginBtnPressed(_ sender: UIButton) {
         
-        performSegue(withIdentifier: TO_LOGIN, sender: nil)
+        if AuthService.instance.isLoggedIn {
+            
+            let profile = ProfileVC()
+            profile.modalPresentationStyle = .custom
+            present(profile, animated: true, completion: nil)
+            
+        } else {
+            
+            performSegue(withIdentifier: TO_LOGIN, sender: nil)
+            
+        }
     }
 }
